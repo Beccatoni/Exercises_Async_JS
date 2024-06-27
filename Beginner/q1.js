c296bb2fd23f555564f900aea0820618237b83cd
Beccatoni <rbccmtni1@gmail.com>
2024-06-27 14:33:02 +0200

    settimeout

---
+++ Beginner/q1.js
@@ -0,0 +1,9 @@
+// 1. Write a function that uses `setTimeout` to log "Hello, World!" to the console after 2 seconds.
+
+function helloWorld(){
+    setTimeout(()=> {
+        console.log('Hello, World!')
+    }, 2000)
+}
+
+helloWorld();
