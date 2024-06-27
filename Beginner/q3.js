ac36f08de43704d284c8a0921df451746c2e44f2
Beccatoni <rbccmtni1@gmail.com>
2024-06-27 14:41:01 +0200

    setInterval

---
+++ Beginner/q3.js
@@ -0,0 +1,9 @@
+//  Using `setInterval`, write a function that logs "Tick" to the console every second.
+
+function setIntervalFun(){
+    setInterval(()=>{
+        console.log('tick')
+    }, 1000)
+}
+
+setIntervalFun();
